bed4eb54f0d2701deafbd1055dc05cdd05d6c804
halfak <aaron.halfaker@gmail.com>
2015-09-24 10:37:22 -0500

    Simplifies the mwxml router with mwcli.Router

--- mwxml/mwxml.py
+++ mwxml/mwxml.py
@@ -1,46 +1,10 @@
-"""
-This script provides access to a set of utilities for extracting content from
-MediaWiki XML dumps.
+import mwcli
 
-* dump2revdocs -- (1) Converts XML dumps to revision documents (XML --> JSON)
+router = mwcli.Router(
+    "mwxml",
+    "This script provides access to a set of utilities for extracting " +
+        "content from MediaWiki XML dumps.",
+    {'dump2revdocs': "Converts XML dumps to revision documents (XML --> JSON)"}
+)
 
-Usage:
-    mwxml -h | --help
-    mwxml <utility> [-h | --help]
-
-Options:
-    -h | --help  Prints this documentation
-    <utility>    The name of the utility to run
-"""
-import sys
-import traceback
-from importlib import import_module
-
-
-USAGE = """Usage:
-    mwxml -h | --help
-    mwxml <utility> [-h | --help]\n"""
-
-
-def main():
-
-    if len(sys.argv) < 2:
-        sys.stderr.write(USAGE)
-        sys.exit(1)
-    elif sys.argv[1] in ("-h", "--help"):
-        sys.stderr.write(__doc__ + "\n")
-        sys.exit(1)
-    elif sys.argv[1][:1] == "-":
-        sys.stderr.write(USAGE)
-        sys.exit(1)
-
-    module_name = sys.argv[1]
-    try:
-        module = import_module(".utilities." + module_name,
-                               package="mwxml")
-    except ImportError:
-        sys.stderr.write(traceback.format_exc())
-        sys.stderr.write("Could not load utility {0}.\n".format(module_name))
-        sys.exit(1)
-
-    module.main(sys.argv[2:])
+main = router.main
