7fc6d8b761e5679cd3d131b56a5b39cf35b63be7
Aaron Halfaker <aaron.halfaker@gmail.com>
2025-02-12 10:54:46 -0800

    Increment version to 0.3.5 (#25)

--- mwxml/about.py
+++ mwxml/about.py
@@ -1,5 +1,5 @@
 __name__ = "mwxml"
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 __author__ = "Aaron Halfaker"
 __author_email__ = "aaron.halfaker@gmail.com"
 __description__ = "A set of utilities for processing MediaWiki XML dump data."
