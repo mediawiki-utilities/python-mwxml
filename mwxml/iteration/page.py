9a17fbccff12f5f701aec33840e152ca789b4a76
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2026-04-08 15:20:39 -0400

    Initialize Page.__revisions to an empty iterator
    This avoids errors when __revisions is not set and aligns with the existing pattern in Dump, where missing iterables are normalized to empty ones.

--- mwxml/iteration/page.py
+++ mwxml/iteration/page.py
@@ -26,18 +26,14 @@ class Page(mwtypes.Page):
         super().initialize(*args, **kwargs)
 
         # Should be a lazy generator
-        self.__revisions = revisions
+        self.__revisions = revisions or iter(())
 
     def __iter__(self):
-        assert self.__revisions is not None
-
         for revision in self.__revisions:
             revision.page = self
             yield revision
 
     def __next__(self):
-        assert self.__revisions is not None
-
         revision = next(self.__revisions)
         revision.page = self
         return revision
